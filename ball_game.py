b4279bb8144f8fd8213327b178bb85ddfe5d96b4
Alrast <ratin.as@phystech.edu>
2020-11-20 23:24:39 +0300

    шарики двигаются их много все можно лопать, добавил квадрат со своим движением и стоимостью

--- ball_game.py
+++ ball_game.py
@@ -1,12 +1,28 @@
 import pygame
+import math
 from pygame.draw import *
 from random import randint
 pygame.init()
 
-FPS = 30
-screen = pygame.display.set_mode((1200, 900))
+FPS = 60
+screen = pygame.display.set_mode((1280, 720))
 
-#colors of balls
+x = randint(100, 700)
+y = randint(100, 500)
+v1 = [-1, 1]
+vx = v1[randint(0, 1)]
+vy = v1[randint(0, 1)]
+
+
+
+
+maxx=1280
+maxy=720
+sum=0
+
+
+
+
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
@@ -16,67 +32,105 @@ CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
-r = 50
-score=0
-score_in=0
-def new_ball():
-    '''рисует новый шарик '''
-    global x, y, r, like_speed_x, like_speed_y, color
-    x = randint(100, 1100)
-    y = randint(100, 900)
-    #r = randint(10, 100)
-    like_speed_x = randint(-1, 1)
-    like_speed_y = randint(-1, 1)
-    color = COLORS[randint(0, 5)]
-    return(x,y,color,like_speed_x,like_speed_y)
+a=randint(2,6)
 
 
-def click(score_in):
-    score_in=0
-    if (((event.pos[0] - x)**2) + (event.pos[1] - y)**2) <= (r**2):
-        score_in+=1
-    else:
-        pass
-    return score_in
+pos_x=[]
+for i in range(a):
+    pos_x.append(0)
+    pos_x[i]=randint(100,700)
 
-pygame.display.update()
+pos_y=[]
+for i in range(a):
+    pos_y.append(0)
+    pos_y[i]=randint(100,700)
+
+pos_r=[]
+for i in range(a):
+    pos_r.append(0)
+    pos_r[i]=randint(30,100)
+color=[]
+for i in range(a):
+    color.append(0)
+    color[i]=COLORS[randint(0, 5)]
+main_paramsmass=[]    
+for i in range(a):
+    main_paramsmass.append(0)
+    main_paramsmass[i]=[pos_x[i],pos_y[i],vx+10*i,vy+10*i,color[i]]
+    
+def click(event):
+    global x, y, r
+    print(x, y, r)
+
+def score(sum):
+    font = pygame.font.Font(None, 25)
+    text = font.render('SCORE ' + str(sum), 1, (255, 255, 255))
+    screen.blit(text, (1180, 10))
+
+
 clock = pygame.time.Clock()
 finished = False
 
-new_ball()
+def ball(x,y,r,color,vx,vy):
+        x += vx
+        y += vy
+        clock.tick(FPS)
+        if x >= (1280 - r):
+            vx *= (-1)
+        elif x <= (0 + r):
+            vx *= (-1)
+        if y >= (720 - r):
+            vy *= (-1)
+        elif y <= (0 + r):
+            vy *= (-1)
+        circle(screen, color, (x, y), r)
+        return [x,y,vx,vy,color]
+vxs=v1[randint(0, 1)]
+vys=vxs
+main_paramss=[randint(100,700),randint(100,700),randint(80,160),randint(80,160),vxs,vys,COLORS[randint(0, 5)]]   
+def squar(x,y,w,h,vxs,vys,color):
+        x += vxs
+        y += 20*vys*math.cos(x*vxs/10)
+        clock.tick(FPS)
+        if x >= (1280 - w):
+            vxs *= (-1)
+        elif x <= 0:
+            vxs *= (-1)
+        if y >= (720 - h):
+            vys *= (-1)
+        elif y <= 0:
+            vys *= (-1)
+        rect(screen, color, (x, y, w, h))
+        return [x,y,w,h,vxs,vys,color]
+        
+
+
 while not finished:
-    clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            finished = True
-            print('Ваши очки', score)
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            score+=click(score_in)
-            print('click',event.pos[0],event.pos[1])
-            print('coords',x,y)
-            if abs(event.pos[0]-x) <= r:
-                if abs(event.pos[1]-y) <= r:
-                    new_ball()
-           
-   
-    pygame.display.update()
-    screen.fill(BLACK)
-    circle(screen, color, (x, y), r)
+    click = False
     
-    if (x-r)>10 and (x+r)<1190 and (y-r)>10 and (y+r)<890:
+
+    while click == False and finished == False:
+        for i in range(len(main_paramsmass)):
+            main_paramsmass[i]=ball(main_paramsmass[i][0],main_paramsmass[i][1],pos_r[i],main_paramsmass[i][4],main_paramsmass[i][2],main_paramsmass[i][3])
+            score(sum)
+        main_paramss=squar(main_paramss[0],main_paramss[1],main_paramss[2],main_paramss[3],main_paramss[4],main_paramss[5],COLORS[randint(0, 5)])
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                finished = True
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                print('Click!')
+                for i in range(len(main_paramsmass)):
+                    if (main_paramsmass[i][0] - event.pos[0]) ** 2 + (main_paramsmass[i][1] - event.pos[1]) ** 2 <= pos_r[i] ** 2:
+                        main_paramsmass[i]=[randint(100, 700),randint(100, 500),v1[randint(0, 1)],v1[randint(0, 1)],COLORS[randint(0, 5)]]
+                        sum += 1
+                        print('счет ', sum)
+                        click = True
+                if event.pos[0]<=(main_paramss[0]+main_paramss[2]) and event.pos[0]>=main_paramss[0] and event.pos[1]>=main_paramss[1] and event.pos[1]<=main_paramss[1]+main_paramss[3]:
+                    main_paramss[4] *= (2)
+                    sum += 3
+                    print('счет ', sum)
+                    click = True
         pygame.display.update()
         screen.fill(BLACK)
-        x += like_speed_x
-        y += like_speed_y
-        circle(screen, color, (x, y), r)
-    elif (x-r)<=10 or (x+r)>=1190:
-        like_speed_x = -like_speed_x
-        x += like_speed_x
-        y += like_speed_y
-    elif (y-r)<=10 or (y+r)>=890:
-        like_speed_y = -like_speed_y
-        x += like_speed_x
-        y += like_speed_y
-
-
 pygame.quit()
